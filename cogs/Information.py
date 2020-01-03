5d6c4cabbdddb85ce06a3ac3c6ee1b45fe34cfdd
TrackRunny <trackrunny@protonmail.com>
2020-01-03 11:05:56 -0800

    Added about command

--- cogs/Information.py
+++ cogs/Information.py
@@ -19,6 +19,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 import random
 
 import discord
+import psutil
 from discord.ext import commands
 
 from logging_files.information_logging import logger
@@ -29,6 +30,49 @@ class Information(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def about(self, ctx):
+        cpu = str(psutil.cpu_percent())
+        ram = str(psutil.virtual_memory()[3] / 1000000000)
+        ram_round = ram[:3]
+        disk = str(psutil.disk_usage('/')[1] / 1000000000)
+        disk_round = disk[:4]
+        boot_time = str(psutil.boot_time() / 100000000)
+        boot_time_round = boot_time[:4]
+
+        source_code = "[**Open source**](https://github.com/TrackRunny/PixelBedrock)"
+        picture = ctx.guild.icon_url_as(size=1024, format=None, static_format="png")
+        emoji = "<:github:662426678907240468>"
+
+        stats = f"```" \
+                f"• CPU: {cpu}%" \
+                f"\n• RAM: {ram_round} / 3 GIGABYTES" \
+                f"\n• DISK: {disk_round} / 40 GIGABYTES" \
+                f"\n• BOOT TIME: {boot_time_round} SECONDS" \
+                f"\n• OPERATING SYSTEM: UBUNTU SERVER 18.04 LTS     " \
+                f"```"
+
+        bot_info = f"```" \
+                   f"• LIBRARY VERSION: DISCORD.PY VERSION 1.2.5" \
+                   f"\n• PYTHON VERSION: 3.7.5     " \
+                   f"```"
+
+        embed = discord.Embed(
+            color=random.choice(self.bot.embed_colors),
+            title="★ PixelBedrock",
+            description=f"\n• Official Discord bot for the PixelBE MCBE server."
+                        f"\n• View **all commands** with `b!commands`"
+                        f"\n• {source_code} {emoji}"
+        )
+        embed.set_thumbnail(url=picture)
+        embed.add_field(name="⇁ Hardware Stats", inline=False, value=stats)
+        embed.add_field(name="⇁ Bot Stats", inline=False, value=bot_info)
+        embed.set_footer(text="• Developed by TrackRunny#0001", icon_url="https://i.imgur.com/TiUqRH8.gif")
+
+        await ctx.send(embed=embed)
+
+        logger.info(f"information | Sent Help: {ctx.author}")
+
     @commands.command(aliases=["help", "cmds"])
     async def commands(self, ctx):
         picture = ctx.guild.icon_url_as(size=1024, format=None, static_format="png")
@@ -38,7 +82,7 @@ class Information(commands.Cog):
         )
         embed.set_thumbnail(url=picture)
 
-        information = "`b!commands`"
+        information = "`b!commands`, `b!about`"
         utility = "`b!uptime`, `b!server`, `b!suggest`"
 
         embed.add_field(name="⇁ Information Commands", inline=False, value=information)
